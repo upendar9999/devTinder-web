1ae82b6e7de25ba387bfb40b9da2da0acd7b3355
upendar9999 <upendar.reddy.musuku@accenture.com>
2024-11-13 22:41:00 +0530

    tailwind + daisyUI + navbar,routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
+import { BrowserRouter, Routes,Route } from "react-router-dom"
+import Body from "./Body"
+import Login from "./Login"
+import Profile from "./Profile"
 
 
 function App() {
 
   return (
-    <>
-      
-      <h1>Hello World</h1>
+    <>     
+        <BrowserRouter basename="/">
+          <Routes>
+             <Route path = "/" element = {<Body/>}>
+             <Route path = "/login" element = {<Login/>}/>
+             <Route path = "/profile" element = {<Profile/>}/>
+             </Route>
+           
+
+          </Routes>
+        </BrowserRouter>
       
     </>
   )
