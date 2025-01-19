ae82dea29527cb0d51f9ea23569a5efe88c59ad3
Musuku Upendar <upendarreddymusuku999@gmail.com>
2025-01-19 17:13:20 +0530

    Removed strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,5 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-  
-  <StrictMode>
     <App />
-  </StrictMode>
 )
